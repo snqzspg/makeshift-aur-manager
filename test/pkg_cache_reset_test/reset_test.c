9a0a279a3c218319d4a38cde7932e7723ba5f26c
michisei <daozheng99+2310@hotmail.com>
2025-01-14 05:33:39 +0800

    Modified git reset tests

--- test/pkg_cache_reset_test/reset_test.c
+++ test/pkg_cache_reset_test/reset_test.c
@@ -3,7 +3,14 @@
 
 #include <aur/pkg_cache.h>
 
+char* pkgs[] = {
+	"nordic-wallpapers-git",
+	"graphite-gtk-theme-git"
+};
+
 int main(void) {
 	(void) fprintf(stderr, "is_pwd_git_repo() returns %s.\n", is_pwd_git_repo() ? "\033[32mtrue\033[0m" : "\033[31mfalse\033[0m");
+
+	git_reset_aur_pkgs(pkgs, sizeof(pkgs) / sizeof(char*));
 	return 0;
 }
