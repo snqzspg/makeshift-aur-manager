f942701c34bfa19ddbdf822adbafedca6aec5e4b
michisei <daozheng99+2310@hotmail.com>
2025-01-14 05:04:15 +0800

    Updated pkg fetch test

--- test/pkg_cache_fetch_test/fetch_test.c
+++ test/pkg_cache_fetch_test/fetch_test.c
@@ -1,5 +1,13 @@
+#include <stdio.h>
+
 #include <aur/pkg_cache.h>
 
+char* pkgs[] = {
+	"nordic-wallpapers-git",
+	"graphite-gtk-theme-git"
+};
+
 int main(void) {
-	return fetch_pkg_base("nordic-wallpapers-git");
+	git_clone_aur_pkgs(pkgs, sizeof(pkgs) / sizeof(char*));
+	return 0;
 }
