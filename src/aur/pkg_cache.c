94a048a408a59b19f00fe0e6c0f6c46b7d1cc13e
michisei <daozheng99+2310@hotmail.com>
2025-01-14 04:15:21 +0800

    Included aur git fetch management

---
+++ src/aur/pkg_cache.c
@@ -0,0 +1,150 @@
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include "../unistd_helper.h"
+
+char* pkg_cache_folder = "__pkg_cache__";
+int   pkg_cache_folder_len = 13;
+
+int create_pkg_cache() {
+	if (mkdir(pkg_cache_folder, S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH) < 0) {
+		if (errno != EEXIST) {
+			(void) fprintf(stderr, "[ERROR][%s:%d]: %s\n", __FILE__, __LINE__ - 2, strerror(errno));
+			return -1;
+		}
+	}
+	return 0;
+}
+
+char does_pkg_cache_exist() {
+	struct stat sb;
+	return stat(pkg_cache_folder, &sb) == 0 && S_ISDIR(sb.st_mode);
+}
+
+size_t write_pkg_base_path(char* __restrict__ dest, size_t limit, const char* pkg_base) {
+	size_t path_len = strlen(pkg_base) + pkg_cache_folder_len + 2;
+	
+	if (dest == NULL) {
+		return path_len - 1;
+	}
+
+	(void) strncat(dest, pkg_cache_folder, limit);
+	(void) strncat(dest, "/", limit);
+	(void) strncat(dest, pkg_base, limit);
+
+	path_len--;
+
+	return limit < path_len ? limit : path_len;
+}
+
+static const char* aur_url_fmt = "https://aur.archlinux.org/%s.git";
+
+int gen_aur_git_url(char* __restrict__ dest, size_t limit, const char* pkg_base) {
+
+	if (dest == NULL) {
+		size_t str_size = snprintf(NULL, 0, aur_url_fmt, pkg_base);
+		return str_size;
+	}
+
+	return snprintf(dest, limit, aur_url_fmt, pkg_base);
+}
+
+char pkg_base_folder_exists(const char* pkg_base) {
+	struct stat sb;
+
+	size_t path_len = strlen(pkg_base) + pkg_cache_folder_len + 2;
+
+	char path[path_len];
+
+	(void) strncat(path, pkg_cache_folder, path_len);
+	(void) strncat(path, "/", path_len);
+	(void) strncat(path, pkg_base, path_len);
+
+	return stat(path, &sb) == 0 && S_ISDIR(sb.st_mode);
+}
+
+char pkg_base_in_cache(const char* pkg_base) {
+	return pkg_base_folder_exists(pkg_base);
+}
+
+int fetch_pkg_base(const char* pkg_base) {
+	if (pkg_base_in_cache(pkg_base)) {
+		return 0;
+	}
+
+	if (create_pkg_cache() < 0) {
+		return -1;
+	}
+
+	size_t url_len = gen_aur_git_url(NULL, 0, pkg_base);
+	char fetch_url[url_len + 1];
+	(void) gen_aur_git_url(NULL, 0, pkg_base);
+
+	size_t dest_len = snprintf(NULL, 0, "%s/%s", pkg_cache_folder, pkg_base);
+	char dest[dest_len + 1];
+	(void) snprintf(dest, dest_len + 1, "%s/%s", pkg_cache_folder, pkg_base);
+
+	int git_subprocess = fork();
+
+	if (git_subprocess == -1) {
+		(void) fprintf(stderr, "[ERROR][%s:%d]: %s\n", __FILE__, __LINE__ - 3, strerror(errno));
+		return -1;
+	} else if (git_subprocess == 0) {
+		(void) fprintf(stderr, "--- \033[1;32mExecuting /usr/bin/git clone %s %s\033[0m ---\n", fetch_url, dest);
+
+		if (execl("/usr/bin/git", "git", "clone", fetch_url, dest, NULL) < 0) {
+			(void) fprintf(stderr, "[NOTE] \033[1;31m/usr/bin/git execution failed!\033[0m\n");
+			(void) fprintf(stderr, "[NOTE] %s\n", strerror(errno));
+			return -1;
+		}
+
+		return -1;
+	} else {
+		int git_proc_stat;
+		run_syscall_print_err_w_ret(waitpid(git_subprocess, &git_proc_stat, 0), -1, __FILE__, __LINE__);
+
+		if (!WIFEXITED(git_proc_stat)) {
+			(void) fprintf(stderr, "[WARNING] pacman exited with code %d.\n", WEXITSTATUS(git_proc_stat));
+			return -1;
+		}
+
+		return 0;
+	}
+}
+
+char is_pkg_in_cache(const char* pkg_name, const char* pkg_base) {
+	if (!does_pkg_cache_exist()) {
+		return 0;
+	}
+
+	if (!pkg_base_in_cache(pkg_base)) {
+		return 0;
+	}
+
+	return 1;
+}
+
+int git_clone_aur_pkgs(char** pkg_bases, size_t n_pkg_bases) {
+	for (size_t i = 0; i < n_pkg_bases; i++) {
+		if (pkg_base_in_cache(pkg_bases[i])) {
+			continue;
+		}
+
+		fetch_pkg_base(pkg_bases[i]);
+	}
+}
+
+int git_reset_aur_pkgs(char** pkg_bases, size_t n_pkg_bases) {
+	for (size_t i = 0; i < n_pkg_bases; i++) {
+		if (pkg_base_in_cache(pkg_bases[i])) {
+			continue;
+		}
+
+		fetch_pkg_base(pkg_bases[i]);
+	}
+}
---
+++ src/aur/pkg_cache.h
@@ -0,0 +1,6 @@
+#ifndef PKG_CACHE_H_INCLUDED
+#define PKG_CACHE_H_INCLUDED
+
+int fetch_pkg_base(const char* pkg_base);
+
+#endif // PKG_CACHE_H_INCLUDED
