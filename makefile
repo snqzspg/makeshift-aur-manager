9a0c14c2084e098ebe686fe9da88887ddb600b18
michisei <daozheng99+2310@hotmail.com>
2025-01-15 06:04:59 +0800

    Linked the fetching to main program

--- makefile
+++ makefile
@@ -4,7 +4,7 @@ LD=gcc
 LDFLAGS=-lcurl 
 
 exec_out=bin/aur_man
-src_files=src/aur_pkg_parse.c src/aur.c src/hashtable.c src/pacman.c src/main.c
+src_files=src/aur/pkg_cache.c src/aur_pkg_parse.c src/aur.c src/hashtable.c src/pacman.c src/main.c
 obj_files=$(patsubst src/%.c, obj/%.o, $(src_files))
 
 .PHONY=run clean
--- src/main.c
+++ src/main.c
@@ -1,15 +1,21 @@
 #include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
 #include "aur.h"
+#include "aur/pkg_cache.h"
 #include "pacman.h"
 #include "hashtable.h"
 #include "unistd_helper.h"
 
 #include "aur_pkg_parse.h"
+
+static int pkg_name_cmp(const void* a, const void* b) {
+	return strcmp(*((char* const*) a), *((char* const*) b));
+}
 
 void aur_list_git(char **pkg_namelist, size_t pkg_namelist_len, hashtable_t installed_pkgs_dict) {
 	(void) write(STDOUT_FILENO, "--- \033[1;36mAUR Git Packages\033[0m ---\n", 36);
@@ -53,6 +59,28 @@ void aur_check_non_git(char **pkg_namelist, size_t pkg_namelist_len, hashtable_t
 		char *col   = found_node -> update_type == UPGRADE ? "\033[32m" : (found_node -> update_type == DOWNGRADE ? "\033[31m" : "");
 		char *arrow = found_node -> update_type == UPGRADE ? "->" : (found_node -> update_type == DOWNGRADE ? "<-" : "==");
 		(void) printf("%s %s\033[1m%s\033[0m %s %s\033[1m%s\033[0m\n", pkg_namelist[i], col, found_node -> installed_ver, arrow, col, found_node -> updated_ver);
+	}
+}
+
+void aur_fetch_non_git_updates(char **pkg_namelist, size_t pkg_namelist_len, hashtable_t installed_pkgs_dict, char **ignore_list, size_t ignore_list_len) {
+	for (size_t i = 0, j = 0; i < pkg_namelist_len; i++) {
+		if (ignore_list != NULL && j < ignore_list_len) {
+			int c = strcmp(pkg_namelist[i], ignore_list[j]);
+			while (j < ignore_list_len && c > 0) {
+				j++;
+			}
+			if (c == 0) {
+				j++;
+				continue;
+			}
+		}
+		struct hashtable_node* found_node = hashtable_find_inside_map(installed_pkgs_dict, pkg_namelist[i]);
+
+		if (found_node -> is_non_aur || found_node -> is_git_package || found_node -> update_type != UPGRADE) {
+			continue;
+		}
+
+		(void) fetch_pkg_base(found_node -> package_base == NULL ? pkg_namelist[i] : found_node -> package_base);
 	}
 }
 
@@ -119,7 +147,7 @@ int aur_check_less_wrap(char **pkg_namelist, size_t pkg_namelist_len, hashtable_
 				(void) fprintf(stderr, "[NOTE] %s\n", strerror(errno));
 				return -1;
 			}
-			
+
 			return -1;
 		} else {
 			run_syscall_print_err_w_ret(close(less_pipe_fds[0]), -1, __FILE__, __LINE__);
@@ -129,6 +157,16 @@ int aur_check_less_wrap(char **pkg_namelist, size_t pkg_namelist_len, hashtable_
 			return 0;
 		}
 	}
+}
+
+void print_usage(const char* arg0) {
+	(void) fprintf(stderr, "usage: %s\n",      arg0);
+	(void) fprintf(stderr, "       %s help\n", arg0);
+	(void) fprintf(stderr, "       %s pacman-checkupdates\n", arg0);
+	(void) fprintf(stderr, "       %s pacman-upgrade\n", arg0);
+	(void) fprintf(stderr, "       %s aur-checkupdates\n", arg0);
+	(void) fprintf(stderr, "       %s aur-fetchupdates\n", arg0);
+	(void) fprintf(stderr, "       %s aur-buildupdates\n", arg0);
 }
 
 int main(int argc, char** argv) {
@@ -159,14 +197,53 @@ int main(int argc, char** argv) {
 
 	const char* response_json = get_packages_info((const char* const*) pkg_namelist, actual_namelist_size);
 
-	size_t res_json_len = strlen(response_json);
-	char mutable_res_json[res_json_len + 1];
-	(void) strncpy(mutable_res_json, response_json, res_json_len + 1);
-	clean_up_pkg_info_buffer();
+	if (response_json != NULL) {
+		size_t res_json_len = strlen(response_json);
+		char mutable_res_json[res_json_len + 1];
+		(void) strncpy(mutable_res_json, response_json, res_json_len + 1);
+		clean_up_pkg_info_buffer();
 
-	process_response_json(mutable_res_json, &installed_pkgs_dict);
+		process_response_json(mutable_res_json, &installed_pkgs_dict);
 
-	(void) aur_check_less_wrap(pkg_namelist, actual_namelist_size, installed_pkgs_dict);
+		if (argc > 1) {
+			if (strcmp(argv[1], "aur-fetchupdates") == 0) {
+				if (argc - 2 > 0) {
+					int n_excludes = (argc - 2) / 2 + (((argc - 2) & 1) != 0);
+					char* excludes[n_excludes];
+
+					for (int i = 2, j = 0; i < argc; i += 2, j++) {
+						if (strcmp(argv[i], "--exclude") != 0) {
+							(void) fprintf(stderr, "[ERROR][aur-fetchupdates] Unrecognized option. Currently only supports --exclude.\n");
+							clean_up_pacman_output();
+							return 1;
+						}
+
+						if (i + 1 >= argc) {
+							(void) fprintf(stderr, "[ERROR][aur-fetchupdates] Missing operand for --exclude.\n");
+							clean_up_pacman_output();
+							return 1;
+						}
+
+						excludes[j] = argv[i + 1];
+					}
+
+					qsort(excludes, n_excludes, sizeof(char*), pkg_name_cmp);
+					aur_fetch_non_git_updates(pkg_namelist, actual_namelist_size, installed_pkgs_dict, excludes, n_excludes);
+				} else {
+					aur_fetch_non_git_updates(pkg_namelist, actual_namelist_size, installed_pkgs_dict, NULL, 0);
+				}
+				clean_up_pacman_output();
+				return 0;
+			}
+		}
+
+		// Need to repeat here inside the if statement or else
+		// the updated version names gets deallocated.
+		(void) aur_check_less_wrap(pkg_namelist, actual_namelist_size, installed_pkgs_dict);
+	} else {
+		(void) aur_check_less_wrap(pkg_namelist, actual_namelist_size, installed_pkgs_dict);
+	}
+
 	// aur_check_non_git(pkg_namelist, actual_namelist_size, installed_pkgs_dict);
 	
 	clean_up_pacman_output();
