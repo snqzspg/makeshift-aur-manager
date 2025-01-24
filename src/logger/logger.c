69e8db7fc20b16cdb877ed785a9ccb1b9d015d06
michisei <daozheng99+2310@hotmail.com>
2025-01-24 18:31:19 +0800

    Modified logger.c to use enum

--- src/logger/logger.c
+++ src/logger/logger.c
@@ -4,111 +4,138 @@
 
 #include "logger.h"
 
-#define TYPE_DEBUG 0
-#define TYPE_INFO 1
-#define TYPE_WARNING 2
-#define TYPE_ERROR 3
-#define TYPE_CRITICAL 4
+// #define TYPE_DEBUG 0
+// #define TYPE_INFO 1
+// #define TYPE_WARNING 2
+// #define TYPE_ERROR 3
+// #define TYPE_CRITICAL 4
 
-char debug_enabled = 0;
-char info_enabled = 0;
-char warning_enabled = 1;
-char error_enabled = 1;
-char critical_enabled = 1;
+// char debug_enabled = 0;
+// char info_enabled = 0;
+// char warning_enabled = 1;
+// char error_enabled = 1;
+// char critical_enabled = 1;
 
-const char BLANK_LABEL[] = "NOTSET";
-const char DEBUG_LABEL[] = "DEBUG";
-const char INFO_LABEL[] = "INFO";
-const char WARNING_LABEL[] = "WARNING";
-const char ERROR_LABEL[] = "ERROR";
+enum __logging_level set_level = NOTE;
+int                  color_set = 0;
+
+const char BLANK_LABEL[]    = "NOTSET";
+const char DEBUG_LABEL[]    = "DEBUG";
+const char INFO_LABEL[]     = "INFO";
+const char NOTE_LABEL[]     = "NOTE";
+const char WARNING_LABEL[]  = "WARNING";
+const char ERROR_LABEL[]    = "ERROR";
 const char CRITICAL_LABEL[] = "CRITICAL";
 
-static const char* get_logging_label(const int logging_type) {
+const char BLANK_COLOURED_LABEL[]    = "\033[1;35mNOTSET\033[0m";
+const char DEBUG_COLOURED_LABEL[]    = "\033[1;32mDEBUG\033[0m";
+const char INFO_COLOURED_LABEL[]     = "\033[1;34mINFO\033[0m";
+const char NOTE_COLOURED_LABEL[]     = "\033[1;36mNOTE\033[0m";
+const char WARNING_COLOURED_LABEL[]  = "\033[1;33mWARNING\033[0m";
+const char ERROR_COLOURED_LABEL[]    = "\033[1;31mERROR\033[0m";
+const char CRITICAL_COLOURED_LABEL[] = "\033[1;31mCRITICAL\033[0m";
+
+static const char* get_logging_label(enum __logging_level logging_type) {
 	switch (logging_type) {
-	case TYPE_DEBUG:
-		return DEBUG_LABEL;
-	case TYPE_INFO:
-		return INFO_LABEL;
-	case TYPE_WARNING:
-		return WARNING_LABEL;
-	case TYPE_ERROR:
-		return ERROR_LABEL;
-	case TYPE_CRITICAL:
-		return CRITICAL_LABEL;
+	case DEBUG:
+		return color_set ? DEBUG_COLOURED_LABEL    : DEBUG_LABEL;
+	case INFO:
+		return color_set ? INFO_COLOURED_LABEL     : INFO_LABEL;
+	case NOTE:
+		return color_set ? NOTE_COLOURED_LABEL     : NOTE_LABEL;
+	case WARNING:
+		return color_set ? WARNING_COLOURED_LABEL  : WARNING_LABEL;
+	case ERROR:
+		return color_set ? ERROR_COLOURED_LABEL    : ERROR_LABEL;
+	case CRITICAL:
+		return color_set ? CRITICAL_COLOURED_LABEL : CRITICAL_LABEL;
 	}
 	return BLANK_LABEL;
 }
 
-static int logging_printf(const int logging_type, const char* fmt, va_list args) {
+int is_logging_type_enabled(enum __logging_level type) {
+    return type >= set_level;
+}
+
+static int logging_vprintf(enum __logging_level logging_type, const char* fmt, va_list args) {
 	const char* label = get_logging_label(logging_type);
-	const size_t extended_fmt_length = strlen(label) + strlen(fmt) + 4;
-	char extended_fmt[extended_fmt_length];
-	(void) strcpy(extended_fmt, "[");
-	(void) strcat(extended_fmt, label);
-	(void) strcat(extended_fmt, "] ");
-	(void) strcat(extended_fmt, fmt);
+	const size_t extended_fmt_length = snprintf(NULL, 0, "[%s]%s", label, fmt);
+	char extended_fmt[extended_fmt_length + 1];
+    (void) snprintf(extended_fmt, extended_fmt_length + 1, "[%s]%s", label, fmt);
+	// (void) strcpy(extended_fmt, "[");
+	// (void) strcat(extended_fmt, label);
+	// (void) strcat(extended_fmt, "] ");
+	// (void) strcat(extended_fmt, fmt);
 	return vfprintf(stderr, extended_fmt, args);
 }
 
-void config_logging(char enable_debug, char enable_info, char enable_warning, char enable_error, char enable_critical) {
-	debug_enabled = enable_debug;
-	info_enabled = enable_info;
-	warning_enabled = enable_warning;
-	error_enabled = enable_error;
-	critical_enabled = enable_critical;
+void config_logging(enum __logging_level set_logging_type, int show_color) {
+	set_level = set_logging_type;
+	color_set = show_color;
 }
 
 int debug_printf(const char* fmt, ...) {
 	va_list args;
-	if (!debug_enabled) {
+	if (!is_logging_type_enabled(DEBUG)) {
 		return 0;
 	}
 	va_start(args, fmt);
-	int ret = logging_printf(TYPE_DEBUG, fmt, args);
+	int ret = logging_vprintf(DEBUG, fmt, args);
 	va_end(args);
 	return ret;
 }
 
 int info_printf(const char* fmt, ...) {
 	va_list args;
-	if (!info_enabled) {
+	if (!is_logging_type_enabled(INFO)) {
 		return 0;
 	}
 	va_start(args, fmt);
-	int ret = logging_printf(TYPE_INFO, fmt, args);
+	int ret = logging_vprintf(INFO, fmt, args);
+	va_end(args);
+	return ret;
+}
+
+int note_printf(const char* fmt, ...) {
+	va_list args;
+	if (!is_logging_type_enabled(NOTE)) {
+		return 0;
+	}
+	va_start(args, fmt);
+	int ret = logging_vprintf(NOTE, fmt, args);
 	va_end(args);
 	return ret;
 }
 
 int warning_printf(const char* fmt, ...) {
 	va_list args;
-	if (!warning_enabled) {
+	if (!is_logging_type_enabled(WARNING)) {
 		return 0;
 	}
 	va_start(args, fmt);
-	int ret = logging_printf(TYPE_WARNING, fmt, args);
+	int ret = logging_vprintf(WARNING, fmt, args);
 	va_end(args);
 	return ret;
 }
 
 int error_printf(const char* fmt, ...) {
 	va_list args;
-	if (!error_enabled) {
+	if (!is_logging_type_enabled(ERROR)) {
 		return 0;
 	}
 	va_start(args, fmt);
-	int ret = logging_printf(TYPE_ERROR, fmt, args);
+	int ret = logging_vprintf(ERROR, fmt, args);
 	va_end(args);
 	return ret;
 }
 
 int critical_printf(const char* fmt, ...) {
 	va_list args;
-	if (!critical_enabled) {
+	if (!is_logging_type_enabled(CRITICAL)) {
 		return 0;
 	}
 	va_start(args, fmt);
-	int ret = logging_printf(TYPE_CRITICAL, fmt, args);
+	int ret = logging_vprintf(CRITICAL, fmt, args);
 	va_end(args);
 	return ret;
 }
--- src/logger/logger.h
+++ src/logger/logger.h
@@ -1,10 +1,21 @@
 #ifndef LOGGER_H_INCLUDED
 #define LOGGER_H_INCLUDED
 
-void config_logging(char enable_debug, char enable_info, char enable_warning, char enable_error, char enable_critical);
+enum __logging_level {
+    DEBUG,
+    INFO,
+    NOTE,
+    WARNING,
+    ERROR,
+    CRITICAL
+};
+
+void config_logging(enum __logging_level set_logging_type, int show_color);
+int is_logging_type_enabled(enum __logging_level type);
 
 int debug_printf(const char* fmt, ...);
 int info_printf(const char* fmt, ...);
+int note_printf(const char* fmt, ...);
 int warning_printf(const char* fmt, ...);
 int error_printf(const char* fmt, ...);
 int critical_printf(const char* fmt, ...);
