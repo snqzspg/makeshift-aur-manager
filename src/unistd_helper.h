9af5ce629eaad4c37549cbd251d7d5528cb52312
michisei <daozheng99+2310@hotmail.com>
2025-01-26 00:39:32 +0800

    Added macro to unistd_helper.h

--- src/unistd_helper.h
+++ src/unistd_helper.h
@@ -16,6 +16,11 @@
 	return errno; \
 }
 
+#define run_syscall_print_err_w_act(syscall_fx, action_statements, log_pf, file_name, line_no) if ((syscall_fx) < 0) {\
+	(void) (log_pf)("[%s:%d]: %s\n", (file_name), (line_no), strerror(errno)); \
+    action_statements \
+}
+
 #define run_syscall_print_w_act(syscall_fx, action_statements, log_type, file_name, line_no) if ((syscall_fx) < 0) {\
 	(void) fprintf(stderr, "[%s][%s:%d]: %s\n", (log_type), (file_name), (line_no), strerror(errno)); \
     action_statements \
