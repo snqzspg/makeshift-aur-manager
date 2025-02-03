a140169e8cae58d65d16b023fd73a26cd942a012
snqzspg <snqzspg@gmail.com>
2025-02-03 17:39:46 +0800

    Added PKGDEST variable init
    Don't know if it will be needed.

--- src/aur/pkg_info.c
+++ src/aur/pkg_info.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 #include "../file_utils.h"
@@ -6,6 +7,9 @@
 #include "../subprocess_unix.h"
 
 #include "pkg_cache.h"
+
+char* pkg_dest           = NULL;
+int   need_free_pkg_dest = 0;
 
 char* get_pkgbase_ver_alloc(const char* pkg_base) {
 	if (!does_pkg_cache_exist() || !pkg_base_in_cache(pkg_base)) {
@@ -65,4 +69,48 @@ char* get_pkg_file_from_pkg_alloc(const char* pkg_name, const char* pkg_base) {
 	streamed_content_t out_stream = stream_fd_content_alloc(ver_out_fd);
 	out_stream.content[out_stream.len - 1] = '\0';
 	return stream_fd_content_detach_str(&out_stream);
+}
+
+int init_pkgdest() {
+	char* env_pkgdest = getenv("PKGDEST");
+	if (env_pkgdest != NULL) {
+		if (need_free_pkg_dest) {
+			free(pkg_dest);
+			need_free_pkg_dest = 0;
+		}
+		pkg_dest = env_pkgdest;
+		return 0;
+	}
+
+	int stream_out_fd;
+	int r = run_subprocess(NULL, 0, 1, &stream_fd_content_alloc, STDIN_FILENO, NULL, "/usr/bin/bash", "bash", "-c", 
+		"source /usr/share/makepkg/util/pkgbuild.sh;"
+		"source /etc/makepkg.conf;"
+	, NULL);
+
+	if (r != 0) {
+		return -1;
+	}
+
+	streamed_content_t out_stream = stream_fd_content_alloc(stream_out_fd);
+	out_stream.content[out_stream.len - 1] = '\0';
+	pkg_dest = stream_fd_content_detach_str(&out_stream);
+	need_free_pkg_dest = 1;
+
+	return 0;
+}
+
+/**
+ * Is NULL if no custom PKGDEST is set.
+ */
+const char* get_pkg_dest() {
+	return pkg_dest;
+}
+
+void deinit_pkgdest() {
+	if (need_free_pkg_dest) {
+		free(pkg_dest);
+		need_free_pkg_dest = 0;
+		pkg_dest = NULL;
+	}
 }
--- src/aur/pkg_info.h
+++ src/aur/pkg_info.h
@@ -4,4 +4,12 @@
 char* get_pkgbase_ver_alloc(const char* pkg_base);
 char* get_pkg_file_from_pkg_alloc(const char* pkg_name, const char* pkg_base);
 
+int init_pkgdest();
+
+/**
+ * Is NULL if no custom PKGDEST is set.
+ */
+const char* get_pkg_dest();
+void deinit_pkgdest();
+
 #endif // PKG_INFO_H_INCLUDED
